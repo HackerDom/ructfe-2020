4c26401a11dda426a3f190f5786e8a4a4b9c5ea8
rx00 <it.tec@me.com>
2020-12-23 02:41:18 +0500

    a bit more stable & randomized checker

--- checkers/mumbler/checker.py
+++ checkers/mumbler/checker.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-
 import requests
 import socket
 import random
 import time
 import uuid
 import string
+from traceback import print_exc
+from user_agent_randomizer import get as get_user_agent
+from requests.exceptions import Timeout
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 from gornilo import \
     GetRequest, \
@@ -13,6 +17,25 @@ from gornilo import \
     PutRequest, \
     Checker, \
     Verdict
+
+
+def requests_with_retry(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(400, 404, 500, 502),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    return session
 
 
 checker = Checker()
@@ -23,8 +46,7 @@ TCP_PORT = 7124
 
 def rand_string(n=32):
     return ''.join(
-        random.SystemRandom()
-            .choice(string.ascii_uppercase + string.digits)
+        random.SystemRandom().choice(string.ascii_uppercase + string.digits)
         for _ in range(n))
 
 
@@ -39,18 +61,24 @@ def check(check_request: CheckRequest) -> Verdict:
             sock.sendto(bytes(f"{data_check}:{rand_data}", "utf-8"), (check_request.hostname, UDP_PORT))
             time.sleep(random.randint(1, 4) * 0.1)
     except Exception as e:
-        print(e)
+        print(e, print_exc())
         return Verdict.DOWN("can't send u anything")
 
     try:
-        resp = requests.get(
-            f"http://{check_request.hostname}:{TCP_PORT}/data/{data_check}")
+        resp = requests_with_retry().get(
+            f"http://{check_request.hostname}:{TCP_PORT}/data/{data_check}",
+            headers={"User-Agent": get_user_agent()},
+            timeout=3
+        )
         if resp.content.decode().strip("\x00") == rand_data:
             return Verdict.OK()
         else:
             return Verdict.CORRUPT("u re sending corrupt data")
+    except Timeout as e:
+        print(e, print_exc())
+        return Verdict.DOWN("service seems not responding")
     except Exception as e:
-        print(e)
+        print(e, print_exc())
         return Verdict.MUMBLE("service mubmbles too hard")
 
 
@@ -66,21 +94,27 @@ def put(put_request: PutRequest) -> Verdict:
             time.sleep(random.randint(1, 4) * 0.1)
         return Verdict.OK(data_check)
     except Exception as e:
-        print(e)
+        print(e, print_exc())
         return Verdict.DOWN("can't send u anything")
 
 
 @checker.define_get(vuln_num=1)
 def get(get_request: GetRequest) -> Verdict:
     try:
-        resp = requests.get(
-            f"http://{get_request.hostname}:{TCP_PORT}/data/{get_request.flag_id.strip()}")
+        resp = requests_with_retry().get(
+            f"http://{get_request.hostname}:{TCP_PORT}/data/{get_request.flag_id.strip()}",
+            headers={"User-Agent": get_user_agent()},
+            timeout=3
+        )
         if resp.content.decode().strip("\x00") == get_request.flag:
             return Verdict.OK()
         else:
             return Verdict.CORRUPT("u re sending corrupt data")
+    except Timeout as e:
+        print(e, print_exc())
+        return Verdict.DOWN("service seems not responding")
     except Exception as e:
-        print(e)
+        print(e, print_exc())
         return Verdict.MUMBLE("service mubmbles too hard")
 
 
