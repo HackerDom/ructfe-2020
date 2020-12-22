8cd3572f01ee1050c87ff62e45e2b206249d3d80
vaspahomov <vas2142553@gmail.com>
2020-12-23 00:25:57 +0500

    fixes

--- checkers/office/api.py
+++ checkers/office/api.py
@@ -1,6 +1,6 @@
 import requests
 
-from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToJson, Parse
 import proto.office_pb2 as pb
 
 
@@ -10,27 +10,55 @@ class Api:
         self.url = f"http://{self.host}"
         self.session = requests.Session()
 
-    def create_doc(self, req: pb.CreateDocumentRequest) -> requests.Response:
-        d = MessageToJson(req)
-        r = self.session.post(f"{self.url}/docs/create", data=d)
-        return r
+    def create_doc(self, req: pb.CreateDocumentRequest) -> (pb.CreateDocumentResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/docs/create", data=d)
+            resp = pb.CreateDocumentResponse()
+            return Parse(r.text, resp), None
+        except Exception as e:
+            return None, str(e)
 
-    def list_doc(self, req: pb.ListDocumentsRequest) -> requests.Response:
-        d = MessageToJson(req)
-        r = self.session.post(f"{self.url}/docs/list", data=d)
-        return r
+    def list_doc(self, req: pb.ListDocumentsRequest) -> (pb.ListDocumentsResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/docs/list", data=d)
+            return Parse(r.text, pb.ListDocumentsResponse()), None
+        except Exception as e:
+            return None, str(e)
 
-    def execute_doc(self, req: pb.ExecuteRequest) -> requests.Response:
-        d = MessageToJson(req)
-        r = self.session.post(f"{self.url}/docs/execute", data=d)
-        return r
+    def execute_doc(self, req: pb.ExecuteRequest) -> (pb.ExecuteResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/docs/execute", data=d)
+            resp = pb.ExecuteResponse()
+            return Parse(r.text, resp), None
+        except Exception as e:
+            return None, str(e)
 
-    def login(self, req: pb.LoginRequest) -> requests.Response:
-        d = MessageToJson(req)
-        r = self.session.post(f"{self.url}/users/login", data=d)
-        return r
+    def login(self, req: pb.LoginRequest) -> (pb.LoginResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/users/login", data=d)
+            resp = pb.LoginResponse()
+            return Parse(r.text, resp), None
+        except Exception as e:
+            return None, str(e)
 
-    def register(self, req: pb.RegisterRequest) -> requests.Response:
-        d = MessageToJson(req)
-        r = self.session.post(f"{self.url}/users/register", data=d)
-        return r
+    def register(self, req: pb.RegisterRequest) -> (pb.RegisterResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/users/register", data=d)
+            resp = pb.RegisterResponse()
+            return Parse(r.text, resp), None
+        except Exception as e:
+            return None, str(e)
+
+    def list_users(self, req: pb.ListRequest) -> (pb.ListResponse, str):
+        try:
+            d = MessageToJson(req)
+            r = self.session.post(f"{self.url}/users/list", data=d)
+            resp = pb.ListResponse()
+            return Parse(r.text, resp), None
+        except Exception as e:
+            return None, str(e)
--- checkers/office/checker.py
+++ checkers/office/checker.py
@@ -28,71 +28,153 @@ Hi '{username}'!
 Bio: '{bio}'"""
 
 
+def create_doc_with_flag(flag: str):
+    return f"""
+vars:
+  sercretinfo: '{flag}'
+---
+Secret info: '{{sercretinfo}}'!!!"""
+
+
 @checker.define_check
 async def check_service(request: CheckRequest) -> Verdict:
     api = Api(f'{request.hostname}:8080')
     req = pb.ListDocumentsRequest()
     req.offset = 0
     req.limit = 100
-    r = api.list_doc(req)
+    r, err = api.list_doc(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
+    req = pb.ListRequest()
+    req.offset = 0
+    req.limit = 100
+    r, err = api.list_users(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
     return Verdict.OK()
 
 
 @checker.define_put(vuln_num=1, vuln_rate=2)
 async def put(request: PutRequest) -> Verdict:
     api = Api(f'{request.hostname}:8080')
-
     name = get_random_str()
     password = get_random_str(30)
-
     req = pb.RegisterRequest()
     req.name = name
     req.password = password
     req.bio = request.flag
+    r, err = api.register(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
 
     req = pb.CreateDocumentRequest()
     req.name = get_random_str()
     req.doc = create_doc()
-    r = api.create_doc(req)
+    r, err = api.create_doc(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
+    try:
+        user_id = r.id
+    except:
+        return Verdict.MUMBLE("failed to get response from service")
+    return Verdict.OK(f"{name}:{password}:{user_id}")
 
-    resp = json.loads(r.text)
-    return Verdict.OK(f"{name}:{password}:{resp['id']}")
+
+@checker.define_put(vuln_num=2, vuln_rate=1)
+async def put_2(request: PutRequest) -> Verdict:
+    api = Api(f'{request.hostname}:8080')
+    name = get_random_str()
+    password = get_random_str(30)
+    req = pb.RegisterRequest()
+    req.name = name
+    req.password = password
+    req.bio = request.flag
+    r, err = api.register(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
+    req = pb.CreateDocumentRequest()
+    req.name = name
+    req.doc = create_doc_with_flag(request.flag)
+    r, err = api.create_doc(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
+    try:
+        user_id = r.id
+    except:
+        return Verdict.MUMBLE("failed to get response from service")
+    return Verdict.OK(f"{name}:{password}:{user_id}")
 
 
 @checker.define_get(vuln_num=1)
 async def get(request: GetRequest) -> Verdict:
     api = Api(f'{request.hostname}:8080')
-
     name, password, id = request.flag_id.split(":")
     req = pb.ExecuteRequest()
-
     req.doc_id = int(id)
     req.username = name
-    r = api.execute_doc(req)
+    r, err = api.execute_doc(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
     try:
-        print(r.text)
-        executed = json.loads(r.text)['executed']
+        executed = r.executed
     except:
         err = "invalid resp format"
         return Verdict.MUMBLE(err)
+
     di, err = DocInfo.parse(executed)
     if err != None:
         return Verdict.MUMBLE(err)
+    if di.bio != request.flag:
+        return Verdict.MUMBLE("invalid flag")
+    return Verdict.OK()
+
+
+@checker.define_get(vuln_num=2)
+async def get_2(request: GetRequest) -> Verdict:
+    api = Api(f'{request.hostname}:8080')
+    name, password, id = request.flag_id.split(":")
+    req = pb.ExecuteRequest()
+    req.doc_id = int(id)
+    req.username = name
+    r, err = api.execute_doc(req)
+    if err != None:
+        print(err)
+        return Verdict.MUMBLE("failed to get response from service")
+    try:
+        executed = r.executed
+    except:
+        err = "invalid resp format"
+        return Verdict.MUMBLE(err)
+    di, err = DocInfo.parse_static(executed)
+    if err != None:
+        return Verdict.MUMBLE(err)
+    if di.secret_info != request.flag:
+        return Verdict.MUMBLE("invalid flag")
     return Verdict.OK()
 
 
 class DocInfo:
     USERNAME_RE = "Hi '(.*)'!"
     BIO_RE = "Bio: '(.*)'"
+    SECRET_RE = "Secret info: '(.*)'!!!"
 
-    def __init__(self, username: str, bio: str):
+    def __init__(self, username: str, bio: str, secret_info: str):
         self.username: str = username
         self.bio: str = bio
+        self.secret_info: str = secret_info
 
     @staticmethod
     def parse(content: str) -> (object, str):
         splitted = content.split('\n')
         if len(splitted) != 2:
+            print(f"invalid lines count was: {len(splitted)} want: 2")
             return None, "invalid doc format"
         try:
             m = re.match(DocInfo.USERNAME_RE, splitted[0])
@@ -100,8 +182,23 @@ class DocInfo:
             m = re.search(DocInfo.BIO_RE, splitted[1])
             bio = m.group(1)
         except Exception as e:
+            print(e)
             return None, "invalid doc format"
-        return DocInfo(username, bio), None
+        return DocInfo(username, bio, ""), None
+
+    @staticmethod
+    def parse_static(content: str) -> (object, str):
+        splitted = content.split('\n')
+        if len(splitted) != 1:
+            print(f"invalid lines count was: {len(splitted)} want: 1")
+            return None, "invalid doc format"
+        try:
+            m = re.match(DocInfo.SECRET_RE, splitted[0])
+            secret_info = m.group(1)
+        except Exception as e:
+            print(e)
+            return None, "invalid doc format"
+        return DocInfo("", "", secret_info), None
 
 
 if __name__ == '__main__':
