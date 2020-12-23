7d53fe3f130ffaddf2fbf2537f912ed4b75c27d3
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2020-12-23 22:59:18 +0500

    checker yescho dolzhen zapuskatsa

--- checkers/carpetradar24/main.py
+++ checkers/carpetradar24/main.py
@@ -10,20 +10,21 @@ def get_random_string(k=10):
 
 
 code = {
-    101: "OK  # operation has been finished sucessfully",
-    102: "CORRUPT  # service is working, but there is no correct flag",
-    103: "MUMBLE  # service is working incorrect (iex: not responding to the protocol)",
-    104: "DOWN  # service not working (iex: no tcp connection can be initialized)",
-    110: "CHECKER_ERROR  # something gone wrong with args or with remote part of checker"
+    101: "OK",
+    102: "CORRUPT",
+    103: "MUMBLE",
+    104: "DOWN",
+    110: "CHECKER_ERROR"
 }
 
 if __name__ == "__main__":
-    verdict = checker.check_service(CheckRequest("localhost"))
+    host = "10.60.4.2"
+    verdict = checker.check_service(CheckRequest(host))
     print(code[verdict._code])
 
-    verdict = checker.put_flag(PutRequest("", "flagflag__flagflag__flagflagflag", 1, "localhost"))
+    verdict = checker.put_flag(PutRequest("", "flagflag__flagflag__flagflagflag", 1, host))
     flag_id = verdict._public_message
     print(code[verdict._code], flag_id)
 
-    verdict = checker.get_flag(GetRequest(flag_id, "flagflag__flagflag__flagflagflag", 1, "localhost"))
+    verdict = checker.get_flag(GetRequest(flag_id, "flagflag__flagflag__flagflagflag", 1, host))
     print(code[verdict._code])
