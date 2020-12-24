f8195453f358891edda87edb9211025c3b591619
amoniaka-knabino <sv@dynfor.ru>
2020-12-24 23:30:23 +0500

    change hex keys to bytes keys in client

--- services/mudarabah/client/client.py
+++ services/mudarabah/client/client.py
@@ -22,7 +22,7 @@ def register(login, password, credit_card_credentials):
     return json.loads(response.read().decode('utf8'))["addition"]
 
 def send_money(token, login_to, amount, description, priv_key):
-    crypter = Crypter.load_private(bytes.fromhex(priv_key))
+    crypter = Crypter.load_private(priv_key)
     new_conditions = {"addition": {"cookie":token, "login_to":login_to, "amount":int(amount), "description":crypter.encrypt(description.encode()).hex()}}
     req = urllib.request.Request(HOST+"/send_money",
                                     data=json.dumps(new_conditions).encode('utf-8'),
@@ -61,16 +61,16 @@ def main():
     if args.register:
         res = register(args.login, args.password, args.credit_card_credentials)
         priv_key_hex = res["priv_key"]
-        with open(f"{args.login}.key", "w") as f:
-            f.write(priv_key_hex)
+        with open(f"{args.login}.key", "wb") as f:
+            f.write(bytes.fromhex(priv_key_hex))
             print(f"key is written to {args.login}.key")
         print(res)
     if args.get_cookie:
         print(get_token(args.login, args.password))
     if args.send_money:
         if args.priv_key_filename:
-            with open(args.priv_key_filename, "r") as f:
-                key = f.read() # should be hex
+            with open(args.priv_key_filename, "rb") as f:
+                key = f.read()
         else:
             key = args.priv_key 
         print(send_money(args.cookie, args.login_to, args.amount, args.description, key))
