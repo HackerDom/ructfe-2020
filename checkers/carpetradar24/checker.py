a54119db4415195789f31361544cf199ec6d1a1b
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2020-12-25 02:23:54 +0500

    more careful checker

--- checkers/carpetradar24/checker.py
+++ checkers/carpetradar24/checker.py
@@ -6,6 +6,7 @@ import string
 from functools import wraps
 from gornilo import Checker, Verdict, CheckRequest, PutRequest, GetRequest
 import traceback
+import requests
 
 checker = Checker()
 
@@ -20,17 +21,19 @@ code = {
     110: "CHECKER_ERROR"
 }
 
+
 def check_exception(func):
     @wraps(func)
     def inner(request, *args, **kwargs) -> Verdict:
         try:
             result = func(request, *args, **kwargs)
-        except ConnectionError:
+        except (ConnectionError, requests.exceptions.RequestException):
             traceback.print_exc()
             result = Verdict.DOWN("Connection error")
         except Exception:
             traceback.print_exc()
-            result = Verdict.CHECKER_ERROR("Internal error")
+            print("Unknown error")
+            result = Verdict.MUMBLE("Something went wrong")
 
         print(f"Return verdict: [{result._code} {code[result._code]}] <{result._public_message}>")
         return result
@@ -44,21 +47,25 @@ def check_service(request: CheckRequest) -> Verdict:
     client = c.Client(request.hostname, TCP_PORT, HTTP_PORT)
 
     login, password = get_creds()
-    token = client.register_and_get_auth_token(login, password)
+    company = get_random_string(15)
+    token = client.register_and_get_auth_token(login, password, company)
     if token is None:
         return Verdict.MUMBLE("Can't register and get auth token")
 
     flight_id = uuid.uuid4()
     label = get_random_string(15)
     license = get_random_string(32)
-    response = client.send_flight_state(token, get_random_int(), get_random_int(), flight_id.bytes, label, license, False)
+    response = client.send_flight_state(token, get_random_int(), get_random_int(), flight_id.bytes, label, license,
+                                        False)
 
     positions = client.get_main_page_positions()
     if positions is None:
         return Verdict.MUMBLE("Can't get or parse main page")
 
     if len(positions) == 0:
-        return Verdict.MUMBLE("Not enough records in positions list")
+        print(f"[---] Positions list is empty")
+        return Verdict.MUMBLE("Not enough rows in positions list")
+    print(f"[+] Positions list has {len(positions)} rows")
 
     try:
         emails = []
@@ -67,13 +74,15 @@ def check_service(request: CheckRequest) -> Verdict:
                 return Verdict.MUMBLE("Not enough values in position row")
             emails.append(p[-1])
     except Exception:
+        print(f"[---] Failed to extract emails from positions list")
         traceback.print_exc()
         return Verdict.MUMBLE("Incorrect positions list")
 
     if f"{login}@carpetradar24.ru" not in emails:
-        print("Not found login on main page")
+        print("[---] Not found login on main page")
         return Verdict.MUMBLE("Service returned incorrect main page")
 
+    print("[+] Found my login on main page")
     return Verdict.OK()
 
 
@@ -83,14 +92,17 @@ def put_flag(request: PutRequest) -> Verdict:
     client = c.Client(request.hostname, TCP_PORT, HTTP_PORT)
 
     login, password = get_creds()
-    token = client.register_and_get_auth_token(login, password)
+    company = get_random_string(15)
+    token = client.register_and_get_auth_token(login, password, company)
     if token is None:
         return Verdict.MUMBLE("Can't register or parse auth token")
 
+    print(f"[+] Registered, will try to send my position")
     flight_id = uuid.uuid4()
     label = get_random_string(15)
     license = request.flag
-    response = client.send_flight_state(token, get_random_int(), get_random_int(), flight_id.bytes, label, license, False)
+    response = client.send_flight_state(token, get_random_int(), get_random_int(), flight_id.bytes, label, license,
+                                        False)
 
     return Verdict.OK(f"{login}:{password}")
 
@@ -109,8 +121,11 @@ def get_flag(request: GetRequest) -> Verdict:
     if flights is None:
         return Verdict.MUMBLE("Can't get or parse chronicle")
 
-    if len(flights) != 1 or len(flights[0]) != 9:
-        return Verdict.MUMBLE("Not enough or too many records in chronicle")
+    if len(flights) < 1 or len(flights[0]) < 8:
+        print(f"[---] Flights list length in chronicle is incorrect")
+        return Verdict.MUMBLE("Not enough records in chronicle")
+    print(f"[+] Flights list has {len(flights)} rows")
+    print(f"[+] Will compare expected and actual flags")
 
     if flights[0][1] != request.flag:
         print(f"Flag '{flights[0][1]}' not equal to expected '{request.flag}'. Creds: {login}:{password}")
