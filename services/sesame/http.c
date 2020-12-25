cbd526410f2706d029a4c46ff6c426c7e3850e40
Krait <krait@dqteam.org>
2020-12-25 23:41:16 +0500

    sesame: added debug output.

--- services/sesame/http.c
+++ services/sesame/http.c
@@ -213,7 +213,8 @@ bool process_request(char *request, char *response, uint64 *response_length)
 		char key[64];
 		bzero(key, sizeof(key));
 
-		for (int i = 0; i < 64; i++)
+		int i = 0;
+		for (i = 0; i < 64; i++)
 		{	
 			gen_key(key, i);
 
@@ -223,6 +224,7 @@ bool process_request(char *request, char *response, uint64 *response_length)
 			store_item(key, secret.data);
 			break;
 		}
+		printf("Sending key: %s for secret: %s, i = %d\n", key, secret.data, i);
 
 		render_page(page, key, secret.data);
 		redirect(response, response_length, key);
